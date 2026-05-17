14c96c84ce4253add8044466a9f99ad16539bc91
AndreyrbSilva <andreyrdh@gmail.com>
2026-05-17 14:54:46 -0300

    refactor: rename app folder to navigation, fix metro bundler conflict

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import "./src/globals.css";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { ThemeProvider } from "./src/context/ThemeContext";
-import { AppNavigator } from "./src/app/index";
+import { AppNavigator } from "./src/navigation/index";
 
 export default function App() {
   return (
